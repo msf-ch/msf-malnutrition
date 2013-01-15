600ac5ca9ebf7e485200b503d794f93b812ed3f3
Nicholas Wilkie <nwilkie@gmail.com>
2013-01-15 01:14:33 -0500

    Moved test classes around
    Test project is now in the main project directory.

---
+++ msf-malnutrition/tests/src/org/msf/android/htmlforms/ImageHtmlForms.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package org.msf.android.htmlforms;
+
+import org.msf.android.activities.malnutrition.MalnutritionWorkflowActivity;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * @author Nicholas Wilkie
+ * 
+ */
+public class ImageHtmlForms extends
+		ActivityInstrumentationTestCase2<MalnutritionWorkflowActivity> {
+
+	/**
+	 * @param pkg
+	 * @param activityClass
+	 */
+	public ImageHtmlForms(Class<MalnutritionWorkflowActivity> activityClass) {
+		super(activityClass);
+		
+	}
+
+}
